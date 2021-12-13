9c4f7868f9469bba91e8bd467f59ff5c735fb4fe
Elizabeth Nathania W <liza_1805@hotmail.com>
2021-12-13 14:43:24 +0900

    update voting contract + add migrations file

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,50 +1,97 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
 
-contract Election {
-    // Model a Candidate
-    struct Candidate {
-        uint id;
-        string name;
-        uint voteCount;
+contract voting {
+  struct Candidate {
+    uint id;
+    string name;
+    uint voteCount;
+  }
+
+  struct Voter {
+    string name;
+    bool voted; //has voted or not
+    bool validated; //voter has registered and passed verification process
+  }
+
+  mapping(address => Voter) public voters;
+  mapping(uint => Candidate) public candidates;
+
+  uint public candCounter;
+  uint public voterCounter;
+
+  constructor() public {
+    addCandidate("Contestant 1");
+    addCandidate("Contestant 2");
+  }
+
+  event votedEvent(uint indexed _candId);
+
+  function addCandidate (string memory _name) private {
+    candCounter++;
+    candidates[candCounter] = Candidate(candCounter,_name,0);
+  }
+
+  function registerVoter(string memory _name, bytes memory signature) public {
+    bytes32 hash = keccak256(abi.encodePacked(_name));
+    address signer = recover(hash, signature);
+    //check signature and check voter hasn't registered before
+    if (signer == msg.sender && !voters[msg.sender].validated) {
+      voterCounter++;
+      voters[msg.sender] = Voter(_name,false,true);
     }
+  }
 
-    // Store accounts that have voted
-    mapping(address => bool) public voters;
-    // Store Candidates
-    // Fetch Candidate
-    mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
-    uint public candidatesCount;
+  function vote (uint _candId) public {
+    //require a registered and valid voters
+    require(voters[msg.sender].validated);
+    //require voters haven't voted before
+    require(!voters[msg.sender].voted);
+    //vote a valid candidate
+    require(_candId > 0 && _candId <= candCounter);
 
-    // voted event
-    event votedEvent (
-        uint indexed _candidateId
-    );
+    voters[msg.sender].voted = true;
+    candidates[_candId].voteCount++;
 
-    constructor () public {
-        addCandidate("Candidate 1");
-        addCandidate("Candidate 2");
-    }
+    emit votedEvent(_candId);
+  }
 
-    function addCandidate (string _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    }
+  /**
+  * @dev Recover signer address from a message by using their signature
+  * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+  * @param signature bytes signature, the signature is generated using web3.eth.sign()
+  */
+  function recover(bytes32 hash, bytes memory signature) public pure returns (address) {
+      bytes32 r;
+      bytes32 s;
+      uint8 v;
 
-    function vote (uint _candidateId) public {
-        // require that they haven't voted before
-        require(!voters[msg.sender]);
+      // Check the signature length
+      if (signature.length != 65) {
+          return (address(0));
+      }
 
-        // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
+      // Divide the signature in r, s and v variables
+      // ecrecover takes the signature parameters, and the only way to get them
+      // currently is to use assembly.
+      // solium-disable-next-line security/no-inline-assembly
+      assembly {
+          r := mload(add(signature, 0x20))
+          s := mload(add(signature, 0x40))
+          v := byte(0, mload(add(signature, 0x60)))
+      }
 
-        // record that voter has voted
-        voters[msg.sender] = true;
+      // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+      if (v < 27) {
+          v += 27;
+      }
 
-        // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
-
-        // trigger voted event
-        emit votedEvent(_candidateId);
-    }
+      // If the version is correct return the signer address
+      if (v != 27 && v != 28) {
+          return (address(0));
+      } else {
+          // solium-disable-next-line arg-overflow
+          return ecrecover(hash, v, r, s);
+      }
+  }
 }

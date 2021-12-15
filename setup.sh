58026941c934e9cdd1735f8e28c180a0c00cec3c
liz7124 <liza_1805@hotmail.com>
2021-12-15 20:44:04 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install -y nodejs
 npm install -g truffle
 npm install -g ganache-cli
+
+npm install express
+npm install eth-crypto
+npm install web3
+npm install express-session
+#npm install --save request-promise-native
